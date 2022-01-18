f92b9d55bd50159078bff3db076e3a7185bc3fe3
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2022-01-18 15:24:18 -0600

    assignment 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Directory from './components/DirectoryComponent';
+import { CAMPSITES } from './shared/campsites';
 
-class App extends Component {
+
+
+
+    class App extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                campsites: CAMPSITES
+            };
+        }
+
+
+
+
+
     render() {
         return (
             <div className="App">
@@ -11,10 +27,12 @@ class App extends Component {
                     <NavbarBrand href="/">NuCamp</NavbarBrand>
                 </div>
                 </Navbar>
+                <Directory campsites={this.state.campsites} />
+
             </div>
         );
     }
-}
 
+    }
 export default App;
 
