6d1ecc4beaa185d9a84e91050381873961f907f7
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2022-02-02 17:12:44 -0600

    intro to redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 import Main from './components/MainComponent';
 
 
@@ -17,11 +18,13 @@ import Main from './components/MainComponent';
 
     render() {
         return (
+            <Provider store={store}>
             <BrowserRouter>
                 <div className="App">
-                  <Main />  
+                    <Main />
                 </div>
             </BrowserRouter>
+        </Provider>
         );
     }
 
