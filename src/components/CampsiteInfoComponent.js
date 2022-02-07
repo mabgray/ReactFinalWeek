11397158679163c3780085e715ac5b7cd5c41f13
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2022-02-07 16:14:51 -0600

    fech from server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -15,6 +15,7 @@ import {
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -107,7 +108,8 @@ function RenderCampsite({ campsite }) {
 	return (
 		<div className="col-md-5 m-1">
 			<Card>
-				<CardImg top src={campsite.image} alt={campsite.name} />
+			
+			<CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
 				<CardBody>
 					<CardText>{campsite.description}</CardText>
 				</CardBody>
